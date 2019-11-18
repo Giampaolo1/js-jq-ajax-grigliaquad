052f410d22fb2ba19a2243effd589299b69c2436
Giampaolo1 <giampaolo.r@outlook.com>
2019-11-18 18:22:57 +0100

    Numeri in pagina

--- script/main.js
+++ script/main.js
@@ -5,20 +5,33 @@
 // se è > di 5 il quadrato diventa verde.
 // Il numero ottenuto appare al centro del quadrato
 
+var elemento = $(".square")
+
 $( document ).ready(function() {
 
   $(".square").click(function(){
+    var clickedSquare = $(this)
     $.ajax({
       url: "https://flynn.boolean.careers/exercises/api/random/int",
       method: "GET",
       success: function(datiRitorno){
-        console.log(datiRitorno);
-        if (response <= 5) {
+        // console.log(datiRitorno);
+        // console.log(clickedSquare);
+
+        if (datiRitorno.response <= 5) {
           // .square background-color:yellow
+          // console.log("Minore di 5");
+          // clickedSquare.style = ("background-color:yellow")
+           clickedSquare.addClass('yellow');
+
+
+        } else {
+          // console.log("Maggiore di 5");
+          clickedSquare.addClass('green');
         }
-        else {
-          // .square background-color:green
-        }
+
+        elemento.html(datiRitorno.response);
+
 
       },
       error: function(){
